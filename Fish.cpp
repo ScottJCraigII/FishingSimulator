9c642c13a1e017f20baa4d9dfc936a0513ab4f4e
Carrie E Adkins <adkinsc2@email.sc.edu>
2019-12-14 10:53:22 -0500

    added comments back

--- Fish.cpp
+++ Fish.cpp
@@ -1,9 +1,14 @@
+//  Written By Scott J. Craig and Carrie E. Adkins
+//  Finished 12/13/2019 11:30 pm
+//  This is a fish simulation  rules listed in read me
+
 #include "Fish.h"
 #include <cstdlib> 
 #include <iostream>
 
 using namespace std;
 
+// default constructor
 Fish :: Fish(){
 	locCol =0;
 	locRow=0;
@@ -12,9 +17,10 @@ Fish :: Fish(){
 	weight=8;
 	size = 'S';
 }
-/**
- *Method allows Pond to place fish randomly within its bounds, 
- */
+
+// constructor that places fish randomly within the 3D grid, assigns sex randomly
+// assigns the starting weight, hunger and size of the fish
+
 Fish :: Fish(int row, int col){
 	locCol = col;
 	locRow = row;
@@ -24,10 +30,15 @@ Fish :: Fish(int row, int col){
 	size = 'S';
 }
 
+// This moves the fish to different locations randomly
+
 void Fish :: swim(){
 	locRow = locRow + ((rand()%3)-1);
 	locCol = locCol + ((rand()%3)-1);
 }
+
+// this causes the fish to grow with the base rate being based on the size before
+// growth, random allows this growth to be different for each fish and season
 
 void Fish :: grow(){
 	if (weight<18) {
@@ -39,6 +50,8 @@ void Fish :: grow(){
 	}
 }
 
+// This sets the size of an inividual fish based on which category it fits
+// fish start S and then grow to M and last grow to L.  Then based on which size the character displayed changes
 void Fish :: setSize(){
 	if(weight/16>=3){ //set ui char based on weight 3lbs is L
 			size='L';
